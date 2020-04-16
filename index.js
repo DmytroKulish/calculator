fe053367d900df8e83b26481fe658270f069c08a
DmytroKulish <62489472+DmytroKulish@users.noreply.github.com>
2020-04-15 23:14:17 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -14,22 +14,22 @@ var bag = Number(document.querySelectorAll("input")[0].value)+Number(document.qu
 if(lastAmount<=fullBatch){
  document.querySelectorAll("input")[5].value = "+" + Math.ceil(amountOfBags) + " bags!"
  document.querySelectorAll("input")[6].value = lastAmount;
- document.querySelectorAll("input")[7].value = (Math.ceil(amountOfBags)*bag).toFixed(3);
+ document.querySelectorAll("input")[7].value = (Math.ceil(amountOfBags)*bag).toFixed(2);
 var poundsReceived =  Number(document.querySelectorAll("input")[7].value)
- document.querySelectorAll("input")[10].value = ((poundsReceived+lastAmount).toFixed(3));
-document.querySelectorAll("input")[11].value = ((poundsReceived+lastAmount).toFixed(3));
- document.querySelectorAll("input")[12].value = ((poundsReceived+lastAmount).toFixed(3));
- document.querySelectorAll("input")[14].value = fullBatch.toFixed(3);
+ document.querySelectorAll("input")[10].value = ((poundsReceived+lastAmount).toFixed(2));
+document.querySelectorAll("input")[11].value = ((poundsReceived+lastAmount).toFixed(2));
+ document.querySelectorAll("input")[12].value = ((poundsReceived+lastAmount).toFixed(2));
+ document.querySelectorAll("input")[14].value = fullBatch.toFixed(2);
   document.querySelectorAll("input")[15].value = numberBatches;
-  document.querySelectorAll("input")[16].value = ((poundsReceived+lastAmount)-fullBatch).toFixed(3);
-    document.querySelectorAll("input")[17].value = ((poundsReceived+lastAmount)-fullBatch).toFixed(3);
+  document.querySelectorAll("input")[16].value = ((poundsReceived+lastAmount)-fullBatch).toFixed(2);
+    document.querySelectorAll("input")[17].value = ((poundsReceived+lastAmount)-fullBatch).toFixed(2);
     document.querySelectorAll("input")[18].value = Math.ceil(amountOfBags) + " mt"
-      document.querySelectorAll("input")[19].value = ((poundsReceived+lastAmount)-fullBatch).toFixed(3);
+      document.querySelectorAll("input")[19].value = ((poundsReceived+lastAmount)-fullBatch).toFixed(2);
 
   document.querySelectorAll("input")[21].value = emptyBag;
 
-  document.querySelectorAll("input")[23].value = (Number(document.querySelectorAll("input")[19].value)-emptyBag*Math.ceil(amountOfBags)).toFixed(3);
-    document.querySelectorAll("input")[24].value = (Number(document.querySelectorAll("input")[19].value)-emptyBag*Math.ceil(amountOfBags)).toFixed(3);
+  document.querySelectorAll("input")[23].value = (Number(document.querySelectorAll("input")[19].value)-emptyBag*Math.ceil(amountOfBags)).toFixed(2);
+    document.querySelectorAll("input")[24].value = (Number(document.querySelectorAll("input")[19].value)-emptyBag*Math.ceil(amountOfBags)).toFixed(2);
 
 
 
@@ -37,10 +37,10 @@ document.querySelectorAll("input")[11].value = ((poundsReceived+lastAmount).toFi
 
 else{
   document.querySelectorAll("input")[6].value = lastAmount;
-    document.querySelectorAll("input")[8].value = fullBatch.toFixed(3);
+    document.querySelectorAll("input")[8].value = fullBatch.toFixed(2);
     document.querySelectorAll("input")[9].value = numberBatches;
-      document.querySelectorAll("input")[10].value = (lastAmount-fullBatch).toFixed(3);
-        document.querySelectorAll("input")[11].value = (lastAmount-fullBatch).toFixed(3);
+      document.querySelectorAll("input")[10].value = (lastAmount-fullBatch).toFixed(2);
+        document.querySelectorAll("input")[11].value = (lastAmount-fullBatch).toFixed(2);
 
 }
 
